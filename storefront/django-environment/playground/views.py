a668601023681535ee37154453d9437ec63f3503
Melad <melad3001@gmail.com>
2023-05-17 15:26:15 -0500

    learning M-V-T model.

--- storefront/django-environment/playground/views.py
+++ storefront/django-environment/playground/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 # request -> response
-# request handler
+# request handlers is what views.py does,
 # action
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x = calculate()
+    return render(request,'hello.html', {'name':'world'})
